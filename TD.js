8ffe6b2b84c4d5b836b5dac5bbf0f2ac756fa9ad
HUSTLER0777 <128897101+HUSTLER0777@users.noreply.github.com>
2023-03-25 17:25:51 +0530

    Update TD.js

--- TD.js
+++ TD.js
@@ -1,55 +1,31 @@
-// Create a "close" button and append it to each list item
-var myNodelist = document.getElementsByTagName("li");
-var i;
-for (i = 0; i < myNodelist.length; i++) {
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  myNodelist[i].appendChild(span);
-}
+function addBtn() {
+  document.getElementById('err').innerHTML = "";
 
-// Click on a close button to hide the current list item
-var close = document.getElementsByClassName("close");
-var i;
-for (i = 0; i < close.length; i++) {
-  close[i].onclick = function() {
-    var div = this.parentElement;
-    div.style.display = "none";
-  }
-}
+  let text = document.getElementById("Task").value;
 
-// Add a "checked" symbol when clicking on a list item
-var list = document.querySelector('ul');
-list.addEventListener('click', function(ev) {
-  if (ev.target.tagName === 'LI') {
-    ev.target.classList.toggle('checked');
-  }
-}, false);
-
-// Create a new list item when clicking on the "Add" button
-function newElement() {
-  var li = document.createElement("li");
-  var inputValue = document.getElementById("myInput").value;
-  var t = document.createTextNode(inputValue);
-  li.appendChild(t);
-  if (inputValue === '') {
-    alert("You must write something!");
+  if (text == '') {
+    document.getElementById('err').innerHTML = "( Please enter something )"
   } else {
-    document.getElementById("myUL").appendChild(li);
-  }
-  document.getElementById("myInput").value = "";
 
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  li.appendChild(span);
+    let box = document.getElementById("myUL")
+    let li = document.createElement("li");
+    li.textContent = text;
+    box.appendChild(li);
 
-  for (i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
-      var div = this.parentElement;
-      div.style.display = "none";
-    }
+    document.getElementById("Task").value = "";
+
+    let x = document.createElement("span");
+    x.textContent = "x";
+    li.appendChild(x);
+
   }
+
+  let btn2 = document.querySelector('ul');
+  btn2.addEventListener('click',function(e){
+    let box = document.getElementById("myUL");
+    let li = e.target.parentNode;
+    box.removeChild(li);
+  })
+
+
 }
